a53bc02c2125fec58fbeee4eab7e18741f2efed9
Nida' Naafilatul <nidanaafilatulh@gmail.com>
2023-10-17 14:00:36 +0700

    configurasi ui

---
+++ resources/views/admin-lte/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  @include('admin-lte.header')
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Preloader -->
+  @include('admin-lte.preloader')
+
+  <!-- Navbar -->
+  @include('admin-lte.navbar')
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  @include('admin-lte.sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+        @include('admin-lte.main-header')
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+        @yield('content')
+  <!-- /.content-wrapper -->
+  @include('admin-lte.footer')
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+@include('admin-lte.javascript')
+</body>
+</html>
